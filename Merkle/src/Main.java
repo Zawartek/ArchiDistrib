ebc7934c26f4b53c2cc96f80e114251d2a50f988
nicosim <nico93simo@hotmail.fr>
2017-11-25 17:11:29 +0100

    Merkle tree from file

--- Merkle/src/Main.java
+++ Merkle/src/Main.java
@@ -1,22 +1,16 @@
 package src;
 
+import java.io.File;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		byte b[] = {0x10};
 		try {
-			MerkleTree mt, mt2,mt3;
-			mt = new MerkleTree(b);
-			System.out.println("MT value : " + mt.getValue());
-			System.out.println("MT encoded value : " + mt.getEncodedValue());
-			mt2 = new MerkleTree(mt,null);
-			System.out.println("MT2 value : " + mt2.getValue());
-			System.out.println("MT2 encoded value : " + mt2.getEncodedValue());
-			mt3 = new MerkleTree(null,mt);
-			System.out.println("MT3 value : " + mt3.getValue());
-			System.out.println("MT3 encoded value : " + mt3.getEncodedValue());
+			MerkleTree mt;
+			File file = new File("DS1-trace.txt");
+			mt = new MerkleTree(file);
+			System.out.println("Final Merkle Tree\n"+mt+"\n");
 			
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
--- Merkle/src/MerkleTree.java
+++ Merkle/src/MerkleTree.java
@@ -1,28 +1,39 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.security.*;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class MerkleTree {
 
 	private static final byte LEAF = 0x00;
-	private static final byte NODE = 0x00;
+	private static final byte NODE = 0x01;
 	private MerkleTree left;
 	private MerkleTree right;
 	private byte[] value;
 	private String encodedValue;
 	private int start;
 	private int end;
+	private int level;
 
-	public MerkleTree(byte[] v) throws NoSuchAlgorithmException {
+	public MerkleTree(String val) throws NoSuchAlgorithmException {
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 		digest.update(LEAF);
-		digest.update(v);
+		digest.update(val.getBytes());
 		value = digest.digest();
+		level = 0;
 		encodedValue = Base64.getEncoder().encodeToString(value);
 	}
 	
 	public MerkleTree(MerkleTree left, MerkleTree right) throws NoSuchAlgorithmException {
+		addNodes(left, right);
+	}
+	private void addNodes(MerkleTree left, MerkleTree right) throws NoSuchAlgorithmException {
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 		this.left = left;
 		this.right = right;
@@ -30,14 +41,59 @@ public class MerkleTree {
 		if (left !=null) {
 			digest.update(left.getValue());
 			start = left.beginIndex();
+			level = left.getLevel() + 1;
+			if (right==null) {
+				end = start;
+			}
 		}
 		if (right !=null) {
 			digest.update(right.getValue());
 			end = right.endIndex();
+			level = right.getLevel() + 1;
+			if (left==null) {
+				start = end;
+			}
 		}
 		value = digest.digest();
 		encodedValue = Base64.getEncoder().encodeToString(value);
 	}
+	
+	public MerkleTree(File file) throws NoSuchAlgorithmException {
+        BufferedReader br;
+        List<MerkleTree> merkleTrees = new ArrayList<MerkleTree>();
+        List<MerkleTree> newMerkleTrees;
+        MerkleTree tree;
+        int cpt=0;
+        String readLine = "";
+
+        try {
+        	br = new BufferedReader(new FileReader(file));
+            while ((readLine = br.readLine()) != null) {
+                tree = new MerkleTree(readLine);
+                tree.setBegin(cpt);
+                tree.setEnd(cpt);
+                merkleTrees.add(tree);
+                cpt++;
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        do {
+        	newMerkleTrees = new ArrayList<MerkleTree>();
+	        for (int i = 0; i<merkleTrees.size(); i+=2) {
+	        	if (merkleTrees.size()-1 == i) {
+	        		newMerkleTrees.add(new MerkleTree(merkleTrees.get(i), null));
+	        	}
+	        	else {
+	        		newMerkleTrees.add(new MerkleTree(merkleTrees.get(i), merkleTrees.get(i+1)));
+	        	}
+	        }
+	        //System.out.println("Merkle Tree "+ (nbLevel++) +"\n" + newMerkleTrees);
+	        merkleTrees = newMerkleTrees;
+        } while(merkleTrees!=null && merkleTrees.size()>2);
+        addNodes(newMerkleTrees.get(0), newMerkleTrees.get(1));
+    }
 	
 	public MerkleTree left() {
 		return left;
@@ -55,8 +111,20 @@ public class MerkleTree {
 		return start;
 	}
 	
+	public void setBegin(int begin) {
+		start = begin;
+	}
+	
 	public int endIndex() {
 		return end;
+	}
+	
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	
+	public int getLevel() {
+		return level;
 	}
 
 	public String getEncodedValue() {
@@ -66,4 +134,19 @@ public class MerkleTree {
 	public void setEncodedValue(String encodedValue) {
 		this.encodedValue = encodedValue;
 	}
+	
+	public String toString() {
+		String s = "";
+		String line = "[" + beginIndex() + "-" + endIndex()+ "] " + getEncodedValue() + "\n";
+		int length = getLevel() + line.length();
+		if (left !=null) {
+			s+= left.toString();
+		}
+		s+= String.format("%1$" + length + "s", line);
+		if (right !=null) {
+			s+= right.toString();
+		}
+		return s;
+	}
+	
 }
